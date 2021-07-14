e8cf25408421a896064ad82bd4b97677923bed92
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-07-13 21:07:06 -0400

    refactored card onTap functions

--- lib/widgets/animal_card_list.dart
+++ lib/widgets/animal_card_list.dart
@@ -4,6 +4,7 @@ import 'package:tcard/tcard.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:pinder/pet_finder.dart' as pet_finder;
 import 'package:pinder/widgets/animal_card.dart';
+import 'package:pinder/config.dart' as config;
 import 'package:location/location.dart';
 
 
@@ -14,10 +15,79 @@ class PinderAnimalCardList extends StatefulWidget {
 
 class _PinderAnimalCardListState extends State<PinderAnimalCardList> {
   TCardController _controller = TCardController();
-  int _index = 0;
   int apiPage = 1;
+  int swipeCount = 0;
   List<PinderAnimalCard> nextAnimalSet = [];
   final List<PinderAnimalCard> animalsList = [];
+  InterstitialAd? _interstitialAd;
+  int _numInterstitialLoadAttempts = 0;
+
+  createCardList(data, list) {
+    data['animals'].forEach((dynamic animal) {
+      PinderAnimalCard card = PinderAnimalCard(
+        metadata: animal,
+        onDislikeTap: () {
+          this._controller.forward(direction: SwipDirection.Left);
+        },
+        onDetailsTap: () {
+          Navigator.pushNamed(context, '/details', arguments: { 'metadata': animal });
+        },
+        onLikeTap: () {
+          this._controller.forward(direction: SwipDirection.Right);
+        },
+      );
+
+      list.add(card);
+    });
+  }
+
+  loadNextAnimalSet() {
+    this.nextAnimalSet.clear();
+
+    getAnimals(++this.apiPage).then((data) {
+      this.createCardList(data, nextAnimalSet);
+    });
+  }
+
+  loadNextAd() {
+    InterstitialAd.load(
+      adUnitId: config.GOOGLE_AD_MOB_INTERSTITIAL_UNIT_ID,
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (InterstitialAd ad) {
+          // Keep a reference to the ad so you can show it later.
+          this._interstitialAd = ad;
+          this._numInterstitialLoadAttempts = 0;
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          print('InterstitialAd failed to load: $error');
+          this._numInterstitialLoadAttempts += 1;
+          this._interstitialAd = null;
+          if (this._numInterstitialLoadAttempts <= config.MAX_AD_LOAD_ATTEMPTS) {
+            this.loadNextAd();
+          }
+        },
+      )
+    );
+  }
+
+  showAd() {
+    _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        print('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+        this.loadNextAd();
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        this.loadNextAd();
+      },
+    );
+
+    _interstitialAd!.show();
+    _interstitialAd = null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,19 +97,9 @@ class _PinderAnimalCardListState extends State<PinderAnimalCardList> {
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           if (snapshot.hasData) { //checks if the response returns valid data
             // Add the widgets to the list
-            getAnimals(++this.apiPage).then((data) {
-              data['animals'].forEach((dynamic animal) {
-                PinderAnimalCard card = PinderAnimalCard(metadata: animal, controller: _controller);
-                nextAnimalSet.add(card);
-              });
-            });
-
-            snapshot.data['animals'].forEach((dynamic animal) {
-              PinderAnimalCard card = PinderAnimalCard(metadata: animal, controller: _controller);
-              animalsList.add(card);
-            });
-
-            print(animalsList.length);
+            this.createCardList(snapshot.data, animalsList);
+            this.loadNextAnimalSet();
+            this.loadNextAd();
 
             // Building the basic UI
             return Container(
@@ -49,34 +109,16 @@ class _PinderAnimalCardListState extends State<PinderAnimalCardList> {
                 controller: _controller,
                 lockYAxis: true,
                 onForward: (index, info) {
-                  _index = index;
-                  //print(info.direction);
-                  //setState(() {});
+                  this.swipeCount++;
+
+                  if (this.swipeCount == 10) {
+                    this.showAd();
+                    this.swipeCount = 0;
+                  }
                 },
                 onEnd: () {
-                  InterstitialAd.load(
-                    adUnitId: 'ca-app-pub-3940256099942544/1033173712',
-                    request: AdRequest(),
-                    adLoadCallback: InterstitialAdLoadCallback(
-                      onAdLoaded: (InterstitialAd ad) {
-                        // Keep a reference to the ad so you can show it later.
-                        //this._interstitialAd = ad;
-                        ad.show();
-                      },
-                      onAdFailedToLoad: (LoadAdError error) {
-                        print('InterstitialAd failed to load: $error');
-                      },
-                    )
-                  );
-
                   _controller.reset(cards: this.nextAnimalSet);
-                  nextAnimalSet.clear();
-                  getAnimals(++this.apiPage).then((data) {
-                    data['animals'].forEach((dynamic animal) {
-                      PinderAnimalCard card = PinderAnimalCard(metadata: animal, controller: _controller);
-                      nextAnimalSet.add(card);
-                    });
-                  });
+                  this.loadNextAnimalSet();
                 },
               ),
             );
